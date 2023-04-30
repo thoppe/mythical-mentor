131ac0111bb92df68772af79336a100596a11639
thoppe <travis.hoppe@gmail.com>
2023-04-30 11:25:16 -0400

    Fix final world

--- Q1_downscale_images.py
+++ Q1_downscale_images.py
@@ -1,12 +1,18 @@
 from pathlib import Path
 from PIL import Image
+from dspipe import Pipe
 
 load_dest = Path("results")
 
+F_TARGET = []
 for f_png in load_dest.glob("**/*.png"):
     f_jpg = f_png.with_suffix(".jpg")
     if f_jpg.exists():
         continue
+    F_TARGET.append(f_jpg)
+
+
+def compute(f_png):
 
     png_image = Image.open(f_png)
 
@@ -16,3 +22,6 @@ for f_png in load_dest.glob("**/*.png"):
     # Save the image as a JPEG file with high quality
     rgb_image.save(f_jpg, quality=85)
     print(f_jpg)
+
+
+Pipe(F_TARGET)(compute, -1)
--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.set_page_config(
 load_dest = Path("results") / "worldbuilding"
 F_JSON = sorted(list(load_dest.glob("**/*.json")))
 
-# @st.cache_resource
+@st.cache_resource 
 def load_data(F_JSON):
     JS = [json.load(open(f)) for f in F_JSON]
     return JS
